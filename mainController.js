1a0286175e3a11416e94f2dac69769a1cdf7be7b
Megha Verma <megha.verma@wingify.com>
2018-05-12 06:07:18 +0530

    daashboraad ready

--- mainController.js
+++ mainController.js
@@ -1,5 +1,6 @@
 angular.module('myApp', []).controller('mainCtrl', function($scope, $http) {
     $scope.showDashboard = true;
+    $scope.loginInfoError = ''
     $scope.names = [
         {name:'Jani',country:'Norway'},
         {name:'Hege',country:'Sweden'},
@@ -9,13 +10,13 @@ angular.module('myApp', []).controller('mainCtrl', function($scope, $http) {
     $scope.fullName = function() {
         return $scope.person.username + " " + $scope.person.password;
     };
-    $scope.validateForm = function() {
+    $scope.validateForm = function () {
         if ($scope.person.username && $scope.person.password) {
             return true;
         }
         return false;
     }
-    $scope.submit = function($event) {
+    $scope.submit = function ($event) {
         $event.preventDefault();
         if($scope.validateForm()){
             $scope.sendPostCall()
@@ -23,24 +24,55 @@ angular.module('myApp', []).controller('mainCtrl', function($scope, $http) {
         return false;
     }
 
-    $scope.sendGetCall = function(url) {
+    var getTokenCookie = function () {
+        var documentCookie = document.cookie;
+        var documentCookieArr = documentCookie.split(';')
+        var returnVal = ''
+        documentCookieArr.forEach(function (elem) {
+            var trimmedElem = elem.trim();
+            trimmedElem = trimmedElem.split('=')
+            if(trimmedElem[0] === 'T') {
+                returnVal = trimmedElem[1]
+            }
+        })
+        return returnVal
+    }
+
+    $scope.sendGetCall = function (url, callback) {
         //$http.get("./response.json" + '?name=' + $scope.username )
-        $http.get(url)
-            .then(function(response) {
-                $scope.data = response.data;
+        var cookie = getTokenCookie()
+        $http.get(url,{
+            'withCredentials': true
+        })
+            .then(function(result) {
+                //$scope.data = result.response[0];
+                //return result;
+                callback(result.data);
         });
 
     }
 
-    $scope.sendPostCall = function(url) {
-        //$http.get("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json" + '?name=' + $scope.username )
-        // $http.get("./response.json" + '?name=' + $scope.username )
-        //     .then(function(response) {
-        //         $scope.data = response.data;
-        // });
+    $scope.modalAction = function (type) {
+        switch (type) {
+            case 'FIR':
+                $scope.modalInfo = {
+                    header: 'FIR',
+                    text: 'File an FIR'
+                }
+                break;
+            case 'Authority':
+                $scope.modalInfo = {
+                    header: 'Contact Authority',
+                    text: 'Contact the concerned authority'
+                }
+                break;
+        }
+    }
+
+    $scope.sendPostCall = function (url) {
         var req = {
             method: 'POST',
-            url: 'http://192.168.2.82:8080/api/login' + '?userName=' + $scope.person.username + '&password=' + $scope.person.password,
+            url: 'https://www.prepchalk.com/api/login' + '?userName=' + $scope.person.username + '&password=' + $scope.person.password,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
             }
@@ -48,29 +80,55 @@ angular.module('myApp', []).controller('mainCtrl', function($scope, $http) {
 
         $http(req)
             .then(function(result){
-                    console.log('success', result)
-                    document.cookie = 't' + '=' + result.data.response.token;
+                    $scope.loginInfoError = '';
+                    $scope.showDashboard = false;
+                    document.cookie = 'T' + '=' + result.data.response.token + '; domain=.prepchalk.com; path=/';
                 },
                 function(error){
-                    console.log('error', error)
+                    $scope.loginInfoError = 'Login Credentials are not authenticated.'
+
                 });
 
     }
 
-    $scope.showServices = function() {
+    $scope.setServiceContent = function (result) {
+        $scope.serviceContent = result.response[0];
+    }
+
+    $scope.getServiceContent = function (serviceName) {
+        //var url = 'http://192.168.2.82:8080/api/user/action/logs?type=' + serviceName;
+        //var url = './response.json?type=' + serviceName;
+        var url = 'https://www.prepchalk.com/api/user/action/logs?type=' + serviceName;
+        $scope.sendGetCall(url, $scope.setServiceContent);
+
+    }
+
+    $scope.showServices = function () {
+        if($scope.loginInfoError) {
+           return ;
+        }
         $scope.showDashboard = false;
         //Array of service names
-        $scope.servicesName = ['Adhaar Card', 'Driving License', 'PanCard', 'Other'];
-        $scope.activeTab = 'Adhaar Card';
+        $scope.servicesName = ['Aadhaar Card', 'Driving License', 'PanCard', 'Other'];
+        $scope.activeTab = 'Aadhaar Card';
         //make call first time to show content of adhaar card
-        $scope.serviceContent = 'Adhaar Card';
+        $scope.getServiceContent('Aadhaar Card');
 
+    }
+    var that = this;
+    this.mappedServiceObject = {
+        'Aadhaar Card': 'aadhar',
+        'Pan Card': 'pan',
+        'Driving License': 'dl',
+        'Other': 'other'
     }
 
     $scope.showTabContent = function(serviceName) {
         $scope.activeTab = serviceName;
+        var mappedService = that.mappedServiceObject[serviceName];
         //make request and show content in $scope.serviceContent
-        $scope.serviceContent = serviceName;
+        $scope.getServiceContent(mappedService);
+       // $scope.serviceContent = serviceName;
     }
 
 
